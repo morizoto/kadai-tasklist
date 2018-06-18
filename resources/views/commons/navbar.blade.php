ad43c5bdf886656b4cfa3d100c34e621ca198494
hiroaki.morise <hiroaki.a.morise@rakuten.com>
2018-06-18 06:05:39 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,11 +11,10 @@
                 </button>
                 <a class="navbar-brand" href="/">Tasklist</a>
             </div>
-            
-            
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                <li>{!! link_to_route('tasks.create', '新規タスクの投稿') !!}</li>
+                <li><a href="#">Signup</a></li>
+                <li><a href="#">Login</a></li>
                 </ul>
             </div>
         </div>
