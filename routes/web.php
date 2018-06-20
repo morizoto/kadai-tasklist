d5f3a0e8bee0236bc4639361693a7aefc2eaa879
hiroaki.morise <hiroaki.a.morise@rakuten.com>
2018-06-20 13:53:22 +0000

    7 login made

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,9 @@ Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('sign
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');               //signup
 
 
-Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');   
-Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
-Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
-
-
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 
 Route::get('/', 'TasksController@index');
